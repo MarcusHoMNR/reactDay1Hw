9f4d2bf1d50833d76f11223867448398156b5b3d
Marcus Ho <marcus.ho@oocl.com>
2021-12-13 17:54:37 +0800

    [ReactDay1HW][marcus] feat: submittedItem

--- src/components/SubmitTodoItem.js
+++ src/components/SubmitTodoItem.js
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 function SubmitTodoItem() {
-  return <div></div>;
+  const [submittedItem, setSubmittedItem] = useState("");
+
+  function onSubmitItem() {
+    setSubmittedItem(document.getElementById("submitInput").value);
+  }
+
+  return (
+    <div>
+      {submittedItem}
+      <input type="text" id="submitInput" name="submitInput" />
+      <input type="submit" value="Submit" onClick={onSubmitItem}></input>
+    </div>
+  );
 }
 
 export default SubmitTodoItem;
