7df001b06691578c6e7f1ab813b5c716599a06e4
Marcus Ho <marcus.ho@oocl.com>
2021-12-14 10:46:39 +0800

    [ReactDay1HW][marcus] fix: code review

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,6 @@
 function TodoItem(props) {
   return (
-    <div className="todoItemRow">
+    <div className="todo-item-row">
       <span style={{ fontSize: "20px" }}>{props.item}</span>
       <br />
     </div>
